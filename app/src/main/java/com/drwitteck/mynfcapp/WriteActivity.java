679de7121130f59db410fe5b6d37f8677a422f79
Derek Witteck <drwitteck@gmail.com>
2018-02-15 13:16:05 -0500

    Write activity

--- app/src/main/java/com/drwitteck/mynfcapp/WriteActivity.java
+++ app/src/main/java/com/drwitteck/mynfcapp/WriteActivity.java
@@ -2,16 +2,21 @@ package com.drwitteck.mynfcapp;
 
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.nfc.FormatException;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
+import android.nfc.tech.Ndef;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
+
+import java.io.IOException;
 
 public class WriteActivity extends AppCompatActivity {
     PendingIntent pi;
@@ -23,6 +28,9 @@ public class WriteActivity extends AppCompatActivity {
 
         Intent intent = new Intent(this, MainActivity.class);
         pi = PendingIntent.getActivity(this, 0, intent, 0);
+
+        findViewById(R.id.link).setOnClickListener((View v) ->
+                startActivity(new Intent(this, WriteActivity.class)));
     }
 
     @Override
@@ -46,7 +54,21 @@ public class WriteActivity extends AppCompatActivity {
 
             Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
 
-            
+            NdefRecord payloadRecord = NdefRecord.createTextRecord(null, payload);
+
+            NdefRecord aarRecord = NdefRecord.createApplicationRecord(getPackageName());
+
+            NdefMessage message = new NdefMessage(new NdefRecord[]{payloadRecord});
+
+            Ndef ndef = Ndef.get(tag);
+
+            try {
+                ndef.connect();
+                ndef.writeNdefMessage(message);
+            } catch (IOException | FormatException e) {
+                e.printStackTrace();
+            }
+
 
         }
 
